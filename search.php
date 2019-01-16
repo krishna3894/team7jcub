6e52b7143807a47f8ee37af3bd08daa99c1b3ac6
Zeusinthecloud <46313074+Zeusinthecloud@users.noreply.github.com>
2019-01-16 14:09:38 +1000

    update

--- search.php
+++ search.php
@@ -3,7 +3,7 @@
  * The template for displaying search results pages.
  *
  * @package team 7 cms
- */
+ */  
 
 get_header(); ?>
 
@@ -22,11 +22,7 @@ get_header(); ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 	
 					<?php
-					/**
-					 * Run the loop for the search to output the results.
-					 * If you want to overload this in a child theme then include a file
-					 * called content-search.php and that will be used instead.
-					 */
+					
 					get_template_part( 'content', 'search' );
 					?>
 	
