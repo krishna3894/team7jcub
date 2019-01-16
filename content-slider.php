147ea6be8a62ffb882879f2da4f0c34b2cfa66df
Zeusinthecloud <46313074+Zeusinthecloud@users.noreply.github.com>
2019-01-16 14:06:02 +1000

    Display for Carousel

--- content-slider.php
+++ content-slider.php
@@ -1,4 +1,4 @@
-<!-- SHOWCASE SLIDER -->
+<!-- Dynamic Slider -->
 <section id="showcase">
     <div id="myCarousel" class="carousel slide " data-ride="carousel">
       <ol class="carousel-indicators">
@@ -6,22 +6,22 @@
         <li data-target="#myCarousel" data-slide-to="1"></li>
         <li data-target="#myCarousel" data-slide-to="2"></li>
       </ol>
-      <div class="carousel-inner">
+      <div  class="carousel-inner">
         <div class="carousel-item carousel-image-1 active">
-          <div class="container">
-            
+          <div class="">
+            <img src="<?php echo wp_get_attachment_url( get_theme_mod('team7cms_slider1_section1'))?>" alt="">
           </div>
         </div>
 
         <div class="carousel-item carousel-image-2">
-          <div class="container">
-            
+          <div class="">
+          <img src="<?php echo wp_get_attachment_url( get_theme_mod('team7cms_slider2_section1'))?>" alt="">
           </div>
         </div>
 
         <div class="carousel-item carousel-image-3">
-          <div class="container">
-            
+          <div class="">
+          <img src="<?php echo wp_get_attachment_url( get_theme_mod('team7cms_slider3_section1'))?>" alt="">
           </div>
         </div>
       </div>
